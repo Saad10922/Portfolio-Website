eecbbe9af143425a818ecc919ad3753662960c3a
saad10922 <saadq10922@gmail.com>
2025-01-27 10:35:01 +0500

    Added configuration files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+     <div className='bg-white h-auto w-full overflow-hidden grid grid-cols-2'  >
+      <span></span>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li></li>
+      </ul>
+     </div>
+  )
+}
+
+export default App
